c979001dc8990f6338d0b666d8b026198e977a50
lindaxiao <lindaxiao.hust@gmail.com>
2016-12-18 13:15:38 +0800

    test

--- chapter 5/GameHelper.java
+++ chapter 5/GameHelper.java
@@ -13,7 +13,7 @@ public class GameHelper{
 				return null;
 			}
 		} catch(IOException e){
-			System.out.println("IOException " + e);
+			System.out.println("IOException: " + e);
 		}
 		return inputLine;
 	}
